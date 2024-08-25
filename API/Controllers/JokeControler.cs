1e87fbaff9f5144dde5115e2372e90e60ae1de83
JK <justask41@gmail.com>
2024-08-25 19:39:13 +0200

    [dev], added branch split after merging in to dev. Monorepo to API and BlazorApp

--- API/Controllers/JokeControler.cs
+++ API/Controllers/JokeControler.cs
@@ -66,7 +66,7 @@ namespace JokeAPI.Controllers
             return Ok(joke);
         }
 
-        [HttpDelete("testing/{id}")]
+        [HttpDelete("aaaassssddd/{id}")]
         public ActionResult<Joke> Delete(int id)
         {
             var joke = _jokeService.DeleteJoke(id);
