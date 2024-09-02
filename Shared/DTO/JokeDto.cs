0350068bed1881b38c3b0cee4cb935e6cd603a81
J <justask41@gmail.com>
2024-09-02 12:00:27 +0200

    [dev], fixes

---
+++ Shared/DTO/JokeDto.cs
@@ -0,0 +1,9 @@
+namespace Shared.DTO
+{
+    public class JokeDTO
+    {
+        public string Content { get; set; }
+        public string Category { get; set; }
+        public int UserId { get; set; }
+    }
+}
